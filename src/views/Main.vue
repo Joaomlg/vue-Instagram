1f4007c7a039250c8ac04335631de7c3f3169fa8
Joao Marcos <joaomarcos1999jm@gmail.com>
2020-05-30 14:12:36 -0300

    View principal

---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="d-flex flex-column justify-center">
+    <publication v-for="i in 10" :key="i" class="mx-auto"></publication>
+  </div>
+</template>
+
+<script>
+import publication from '../components/publication'
+
+export default {
+  components: { publication }
+}
+</script>
