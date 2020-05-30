892d2124e8f7be2b3d2c0eb696d6cc4c243ffec0
Joao Marcos <joaomarcos1999jm@gmail.com>
2020-05-30 16:10:13 -0300

    Dinamic publications

--- src/components/publication.vue
+++ src/components/publication.vue
@@ -3,17 +3,19 @@
     <v-card-title class="py-0 px-0">
       <v-container>
         <v-row align="center">
-          <v-avatar class="ml-3" color="grey" size="36px"></v-avatar>
+          <v-avatar class="ml-3" size="38px" color="grey lighten-3">
+            <v-img :src="avatar_img"></v-img>
+          </v-avatar>
           <v-col cols="8" class="py-0">
-            <span class="d-block subtitle-2 black--text">João Marcos</span>
-            <span class="d-block caption black--text">Conselheiro Lafaiete</span>
+            <span class="d-block subtitle-2 black--text">{{ user }}</span>
+            <span class="d-block caption black--text">{{ local }}</span>
           </v-col>
           <v-spacer></v-spacer>
-          <v-btn icon class="mr-3"><v-icon color="icons">mdi-dots-horizontal</v-icon></v-btn>
+          <v-btn icon class="mr-3"><v-icon color="icons" v-text="$isMobile ? 'mdi-dots-vertical' : 'mdi-dots-horizontal'"></v-icon></v-btn>
         </v-row>
       </v-container>
     </v-card-title>
-    <v-img max-height="300px" contain src="https://picsum.photos/500/300"></v-img>
+    <v-img max-height="300px" :src="url"></v-img>
     <v-card-text class="py-0 px-0">
       <v-container>
         <v-row class="px-3 pb-1">
@@ -24,8 +26,8 @@
           <v-icon color="icons">mdi-bookmark-outline</v-icon>
         </v-row>
         <v-row class="px-3">
-          <span class="subtitle-2 black--text mr-3">João Marcos</span>
-          <span class="caption black--text text-wrap">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+          <span class="subtitle-2 black--text mr-3">{{ user }}</span>
+          <span class="caption black--text text-wrap">{{ description }}</span>
         </v-row>
         <v-divider class="mt-3 mb-1"></v-divider>
         <v-row align="center" class="px-3">
@@ -42,6 +44,11 @@
 
 <script>
 export default {
-  
+  props: ['user', 'local', 'description', 'url'],
+  computed: {
+    avatar_img() {
+      return `https://avatars.dicebear.com/api/bottts/${this.$props.user}.svg`
+    }
+  }
 }
 </script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="d-flex flex-column justify-center">
-    <publication v-for="i in 10" :key="i" class="mx-auto"></publication>
+    <publication v-for="(item, i) in items" :key="i" 
+      class="mx-auto"
+      :user="item.author"
+      
+      :description="item.url"
+      :url="item.download_url"
+    ></publication>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import publication from '../components/publication'
 
 export default {
-  components: { publication }
+  components: { publication },
+  data: () => ({
+    items: []
+  }),
+  created() {
+    for (let i=0; i<10; i++) {
+      axios.get(`https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/info`)
+      .then(response => {
+        this.items.push(response.data);
+      })
+    }
+  }
 }
 </script>
